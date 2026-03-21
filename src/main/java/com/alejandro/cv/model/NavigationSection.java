f68d884de36b5f18f3e44a36aa61aee794cfcbdd
AlejoValenciaR <alejo.valenciarivera@gmail.com>
2026-03-20 21:28:01 -0500

    RESET PROJECT FROM BEGINNING

--- src/main/java/com/alejandro/cv/model/NavigationSection.java
+++
@@ -1,8 +0,0 @@
-package com.alejandro.cv.model;
-
-import java.util.List;
-
-public record NavigationSection(List<Item> items) {
-    public record Item(String targetId, String label, String iconClass) {
-    }
-}
--- src/main/java/com/alejandro/hello_spring/HelloSpringApplication.java
+++ src/main/java/com/alejandro/hello_spring/HelloSpringApplication.java
@@ -2,10 +2,8 @@ package com.alejandro.hello_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.alejandro")
 public class HelloSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
